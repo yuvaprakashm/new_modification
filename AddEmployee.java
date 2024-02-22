e2fbe12c5f8d74368ef1ff2ba7975a2ecb071a38
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-22 17:14:12 +0530

    Add files via upload

---
+++ AddEmployee.java
@@ -0,0 +1,53 @@
+package net.texala.employee.impl;
+
+import java.util.Scanner;
+import net.texala.employee.manager.EmployeeManager;
+import net.texala.employee.model.Employee;
+import net.texala.employee.service.EmployeeOperation;
+import net.texala.employee.exceptions.EmployeeOperationException;
+import static net.texala.employee.constants.Constants.ADD_EMPLOYEE_HEADER_TOP;
+import static net.texala.employee.constants.Constants.ADD_EMPLOYEE_HEADER_MIDDLE;
+import static net.texala.employee.constants.Constants.ADD_EMPLOYEE_HEADER_BOTTOM;
+import static net.texala.employee.constants.Constants.ENTER_EMPID;
+import static net.texala.employee.constants.Constants.ENTER_FIRST_NAME;
+import static net.texala.employee.constants.Constants.ENTER_LAST_NAME;
+import static net.texala.employee.constants.Constants.ERROR_ADD_EMPLOYEE;
+
+public class AddEmployee implements EmployeeOperation {
+	 
+    @Override
+    public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
+        try {
+        	System.out.println(ADD_EMPLOYEE_HEADER_TOP);
+        	System.out.println(ADD_EMPLOYEE_HEADER_MIDDLE);
+        	System.out.println(ADD_EMPLOYEE_HEADER_BOTTOM);
+        	manager.loadEmployees();
+
+            System.out.print(ENTER_EMPID);
+            int empId = manager.getValidEmpId(scanner);
+            
+            if (manager.isEmployeeIdExists(manager, empId)) {
+            	System.out.println("Error: Employee with ID " + empId + " already exists.");
+                manager.temporaryVector.clear();  
+                return;
+            }
+            
+            System.out.print(ENTER_FIRST_NAME);
+            String firstName = scanner.nextLine();
+
+            System.out.print(ENTER_LAST_NAME);
+            String lastName = scanner.nextLine();
+
+            String department = manager.selectDepartment(scanner);
+
+            System.out.println();
+            manager.addEmployee(empId, firstName, lastName, department);
+            manager.temporaryVector.add(new Employee(empId, firstName, lastName, department));
+        } catch (IllegalArgumentException e) {
+            System.out.println("\nError: " + e.getMessage());
+        } catch (Exception e) {
+            EmployeeOperationException.throwAddEmployeeException(ERROR_ADD_EMPLOYEE + e.getMessage());
+        }
+    }
+ 
+}
