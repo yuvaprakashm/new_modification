7a87d73a334018f5812e1a3f0a81430a267baa49
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-02-23 17:23:17 +0530

    Update DisplayEmployee.java

--- DisplayEmployee.java
+++ DisplayEmployee.java
@@ -16,59 +16,55 @@ import static net.texala.employee.constants.Constants.DISPLAY_OPTION_EMPID;
 import static net.texala.employee.constants.Constants.DISPLAY_OPTION_SORTED;
 import static net.texala.employee.constants.Constants.DISPLAY_OPTION_MEMORY;
 import static net.texala.employee.constants.Constants.ENTER_CHOICE;
- 
-
-
 
 public class DisplayEmployee implements EmployeeOperation {
-    @Override
-    public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
-    	try {
-            
-    		 manager.loadEmployees();
+	@Override
+	public void execute(EmployeeManager manager, Scanner scanner) throws EmployeeOperationException {
+		try {
 
-            boolean validInput = false;
-            while (!validInput) {
-            	System.out.println(ADD_EMPLOYEE_HEADER_TOP);
-            	System.out.println(DISPLAY_MENU_HEADER);
-                System.out.println(ADD_EMPLOYEE_HEADER_BOTTOM);
-                System.out.println(DISPLAY_OPTION_EMPID);
-                System.out.println(DISPLAY_OPTION_SORTED);
-                System.out.println(DISPLAY_OPTION_MEMORY);  
-                System.out.print(ENTER_CHOICE);
-                int displayOption = scanner.nextInt();
-                scanner.nextLine();
+			boolean validInput = false;
+			while (!validInput) {
+				System.out.println(ADD_EMPLOYEE_HEADER_TOP);
+				System.out.println(DISPLAY_MENU_HEADER);
+				System.out.println(ADD_EMPLOYEE_HEADER_BOTTOM);
+				System.out.println(DISPLAY_OPTION_EMPID);
+				System.out.println(DISPLAY_OPTION_SORTED);
+				System.out.println(DISPLAY_OPTION_MEMORY);
+				System.out.print(ENTER_CHOICE);
+				int displayOption = scanner.nextInt();
+				scanner.nextLine();
 
-                switch (displayOption) {
-                    case 1:
-                        System.out.println(ENTER_EMPID);
-                        int empId = scanner.nextInt();
-                        scanner.nextLine();
-                        Employee employee = manager.getEmployeeByEmpId(empId, manager.temporaryVector);
-                        if (employee == null) {
-                            System.out.println(EMP_NOTFOUND);
-                        } else {
-                            System.out.println(employee);
-                        }
-                        manager.temporaryVector.clear();
-                        validInput = true;
-                        break;
-                    case 2:
-                        manager.displaySortedRecordsOption(manager, scanner);
-                        validInput = true;
-                        break;
-                    case 3:
-                        manager.displayAllEmployeesWithOption(scanner);  
-                        validInput = true;
-                        break; 
-                    default:
-                        System.out.println(INVALID_OPTION_SELECTED);
-                        break;
-                }
-            }
-            } catch (Exception e) {
-                System.out.println(ERROR_INVALID_INPUT);
-                scanner.nextLine();
-            }
-        }
-    }
+				switch (displayOption) {
+				case 1:
+					System.out.println(ENTER_EMPID);
+					int empId = scanner.nextInt();
+					scanner.nextLine();
+					Employee employee = manager.getEmployeeByEmpIdDisplay(empId, manager.temporaryVector);
+					if (employee == null) {
+						System.out.println(EMP_NOTFOUND);
+					} else {
+						System.out.println(employee);
+					}
+					manager.temporaryVector.clear();
+					validInput = true;
+					break;
+				case 2:
+
+					manager.displaySortedRecordsOption(manager, scanner);
+					validInput = true;
+					break;
+				case 3:
+					manager.displayAllEmployeesWithOption(scanner);
+					validInput = true;
+					break;
+				default:
+					System.out.println(INVALID_OPTION_SELECTED);
+					break;
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(ERROR_INVALID_INPUT);
+			scanner.nextLine();
+		}
+	}
+}
